a1c095468ceefc29ac7c6e8801982e76e0d38738
Roman Czerwinski <romanczerwinski@r0m4n.com>
2023-07-29 19:24:10 -0500

    updated te join command

--- debian/kubernetes-node/restart-node.sh
+++ debian/kubernetes-node/restart-node.sh
@@ -12,15 +12,21 @@ echo-restart-node-help() {
     echo '|'
     echo '| Params'
     echo '| ------'
-    echo '| $1 : string : JOIN_TOKEN'
+    echo '| $1 : string : IP_AND_PORT'
+    echo '| The IP and port of the parent node to join'
+    echo '|'
+    echo '| $2 : string : TOKEN'
     echo '| The kubeadm join token to use to join the cluster'
+    echo '|'
+    echo '| $3 : string : DISCOVERY_CA_CERT_HASH'
+    echo '| The discovery cert hash for the join command'
     echo '|'
 }
 
 
 main() {
     case "$#" in
-        "$1" )
+        "$3" )
             case "$1" in
                 'h' | 'help' | '-h' | '--help' )
                     echo-restart-node-help
@@ -33,10 +39,16 @@ main() {
             return 1
             ;;
     esac
+    IP_AND_PORT="$1"
+    TOKEN="$2"
+    DISCOVERY_CA_CERT_HASH="$3"
     sudo swapoff -a
     sudo kubeadm reset
     sudo rm -r /etc/cni/net.d
-    sudo kubeadm join $@
+    sudo kubeadm join \
+        "${IP_AND_PORT}" \
+        --token "${TOKEN}" \
+        --discovery-token-ca-cert-hash "${DISCOVERY_CA_CERT_HASH}"
 }
 
 main $@
