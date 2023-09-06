d435ad8415ebe084d4c54c3dcfb83a28e1fe3f9a
Roman Czerwinski <romanczerwinski@r0m4n.com>
2023-09-06 07:47:02 -0400

    changed based on latest k8s install guides

--- debian/headless/init.sh
+++ debian/headless/init.sh
@@ -31,13 +31,17 @@ main() {
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+    # Updated per
+    # https://kubernetes.io/docs/setup/production-environment/container-runtimes/
     cat <<EOF | tee /etc/modules-load.d/k8s.conf
 br_netfilter
+overlay
 EOF
 
     cat <<EOF | tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 EOF
     
     sysctl --system
