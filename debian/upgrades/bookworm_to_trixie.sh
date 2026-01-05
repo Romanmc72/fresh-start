18a842791da957983fd8bc15cfc82eaf17f5293b
Roman Czerwinski <romanczerwinski@r0m4n.com>
2026-01-04 19:51:55 -0600

    remove trailing newline character

--- debian/upgrades/bookworm_to_trixie.sh
+++ debian/upgrades/bookworm_to_trixie.sh
@@ -17,7 +17,7 @@ main() {
     apt full-upgrade -y
     apt --purge autoremove -y
     sed -i 's/bookworm/trixie/g' /etc/apt/sources.list
-    find /etc/apt/sources.list.d -type f -exec sed -i 's/bookworm/trixie/g' {} \;\n
+    find /etc/apt/sources.list.d -type f -exec sed -i 's/bookworm/trixie/g' {} \;
     apt update
     apt upgrade -y
     apt full-upgrade -y
